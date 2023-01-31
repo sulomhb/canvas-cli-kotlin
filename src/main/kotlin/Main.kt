f721e8dc132504d883740d976f7496bddd5b6b6b
sulomhb <sulomhb@gmail.com>
2023-01-31 10:55:22 +0100

    added .env for token

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
+
+import io.github.cdimascio.dotenv.Dotenv
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-
-/* Canvas Token : 10742~K6jARCPU2szlPBT1nX2emMWyRfxRgC6qZoo1NV1fZ53JFjKivY0xfxHWvxwGmDyf */
 fun main() {
-    val client = HttpClient.newBuilder().build();
+    val dotenv = Dotenv.load();
+    val token = dotenv.get("CANVAS_TOKEN");
 
+    val client = HttpClient.newBuilder().build();
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://uia.instructure.com/api/v1/courses/"))
-        .header("Authorization", "Bearer 10742~K6jARCPU2szlPBT1nX2emMWyRfxRgC6qZoo1NV1fZ53JFjKivY0xfxHWvxwGmDyf")
+        .header("Authorization", "Bearer $token")
         .build();
     val response = client.send(request, HttpResponse.BodyHandlers.ofString());
     println(response.body())
