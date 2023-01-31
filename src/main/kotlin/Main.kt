a645ac95dae61a23664361991838c816054b15ad
sulomhb <sulomhb@gmail.com>
2023-01-31 10:00:26 +0100

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,7 @@ import java.net.http.HttpResponse
 /* Canvas Token : 10742~K6jARCPU2szlPBT1nX2emMWyRfxRgC6qZoo1NV1fZ53JFjKivY0xfxHWvxwGmDyf */
 fun main() {
     val client = HttpClient.newBuilder().build();
+
     val request = HttpRequest.newBuilder()
         .uri(URI.create("https://uia.instructure.com/api/v1/courses/"))
         .header("Authorization", "Bearer 10742~K6jARCPU2szlPBT1nX2emMWyRfxRgC6qZoo1NV1fZ53JFjKivY0xfxHWvxwGmDyf")
