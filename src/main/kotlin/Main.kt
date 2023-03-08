6b5ca966479399f2265a66c98c591e66e302bd17
Suleyman Selcuk <sulomhb@grm-studby-128-39-148-217.studby.uia.no>
2023-03-08 03:20:30 +0100

    fetch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,15 +5,21 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-fun main() {
+
+fun fetch_API(url : String) {
     val dotenv = Dotenv.load();
     val token = dotenv.get("CANVAS_TOKEN");
 
     val client = HttpClient.newBuilder().build();
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://uia.instructure.com/api/v1/courses/"))
+        .uri(URI.create(url))
         .header("Authorization", "Bearer $token")
         .build();
     val response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    println(response.body())
+    return response.body()
+ }
+
+fun main() {
+    fetch_API("https://uia.instructure.com/api/v1/courses/");
+    println("Test");
 }
